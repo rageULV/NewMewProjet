8e519909f31d8344c00d8ce2ee00dc88072f4c67
rageULV <esawein5@gmail.com>
2024-03-08 14:45:30 +0200

    1
    1

--- src/test/java/tests/TestClass.java
+++ src/test/java/tests/TestClass.java
@@ -13,10 +13,10 @@ public class TestClass extends BaseTest {
     public void test001() throws InterruptedException {
        HomePage hp = new HomePage(getDriver());
        LogInPage logInPage = hp.openTopMenu(TopMenuItem.LOGIN);
-       logInPage.tryToFindAnElement();
-       LetTheCarWorkPage lcwp = logInPage.openTopMenu(TopMenuItem.LET_THE_CAR_WORK);
-       lcwp.setManufacturer("BMW").addLocation("Montana")
-               .setFuelType().photoAttachment("C:\\Pictures\\BlueCar.jpg");
+//       logInPage.tryToFindAnElement();
+//       LetTheCarWorkPage lcwp = logInPage.openTopMenu(TopMenuItem.LET_THE_CAR_WORK);
+//       lcwp.setManufacturer("BMW").addLocation("Montana")
+//               .setFuelType().photoAttachment("C:\\Pictures\\BlueCar.jpg");
 
 
        Thread.sleep(3000);
